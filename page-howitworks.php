99a06ba5e09fb576e4689d68410078b7c806e33c
Tim Spinks <tim@ninthlink.com>
2013-06-26 17:21:21 +0000

    adding how it works page

---
+++ page-howitworks.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template Name: How It Works
+ *
+ *
+ * @package SoFi
+ * @subpackage Sofi
+ * @since SoFi 1.0
+ */
+
+get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it. ?>
+		<div id="page-top" class="page wimg hfeed">
+		<?php the_post_thumbnail('full'); ?>
+		
+			<div class="header wimg">
+				<h1 class="title wimg"><?php echo get_the_title(); ?></h1>
+				<h4 class="title wimg"><?php echo get_post_meta( get_the_ID(), 'sub-title', true ); ?></h4>
+				<?php /* the school widget here */ ?>
+			</div>
+		</div>
+	<?php } else { ?>
+		<div id="page-top" class="page hfeed default-page">
+			<div class="header">
+				<h1 class="title"><?php echo get_the_title(); ?></h1>
+			</div>
+		</div>
+	<?php } ?>
+
+<div class="page page-howitworks hfeed">
+	<div class="site">
+		<?php get_sidebar('howitworks'); ?>
+	</div>
+</div>
+
+<div class="page" class="page hfeed">
+	<div class="site">
+		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+			<div class="site-content">
+				<?php the_content(); ?>
+			</div>
+			<?php get_sidebar(); ?>
+		<?php else : ?>
+			<?php the_content(); ?>
+		<?php endif; ?>
+	</div>
+</div>
+
+<?php endwhile; // end of the loop. ?>
+
+<div class="page hfeed">
+	<div class="site">
+
+<?php get_footer(); ?>
